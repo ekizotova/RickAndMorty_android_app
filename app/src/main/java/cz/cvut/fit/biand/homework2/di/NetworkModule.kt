81d2055c4abe46081d82e71825153d90a7f41e4a
Ekaterina <ekaterina.izotova.work@gmail.com>
2025-05-21 12:03:04 +0200

    Create NetworkModule.kt

---
+++ app/src/main/java/cz/cvut/fit/biand/homework2/di/NetworkModule.kt
@@ -0,0 +1,34 @@
+package cz.cvut.fit.biand.homework2.di
+
+import cz.cvut.fit.biand.homework2.api.CharacterApi
+import dagger.*
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideCharacterApi(client: HttpClient): CharacterApi {
+        return CharacterApi(client)
+    }
+}
